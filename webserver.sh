20d26a15cfadd45f525c3f67ade6196764ffa6f8
ktomi96 <kokai.tamas.96@gmail.com>
2023-03-01 22:11:03 +0100

    Reduced docker image size, using gunicorn for wsgi.

--- webserver.sh
+++ webserver.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v $(pwd)/env:/app/env -v $(pwd)/logs:/app/logs -v $(pwd)/database:/app/database --name webserver webserver
+docker run --rm -v $(pwd)/env:/app/env -v $(pwd)/logs:/app/logs -v $(pwd)/database:/app/database -p 5000:5000 --name webserver webserver
